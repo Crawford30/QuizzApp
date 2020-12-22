9a1b017afcc65e7f75cd3aa604e339cae663110b
joel <joelcrawford30@gmail.com>
2020-12-22 10:24:25 +0300

    hide status bar and finish activity

--- app/src/main/java/com/example/quizzapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizzapp/ResultActivity.kt
@@ -3,6 +3,8 @@ package com.example.quizzapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import kotlinx.android.synthetic.main.activity_result.*
 import kotlinx.android.synthetic.main.activity_result.view.*
 
@@ -10,6 +12,9 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        //hide status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         val username = intent.getStringExtra(Constants.USER_NAME)
         tv_name.text = username
@@ -21,6 +26,9 @@ class ResultActivity : AppCompatActivity() {
 
         btn_finish.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
+
+            //close the current activity
+            finish()
         }
 
 
